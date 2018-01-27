27d01fefb60ced411da3f2653d9f61593a35f96e
Xu Deng <d8660091@gmail.com>
2018-01-27 15:28:22 -0500

    Use npm packge for story book

--- .storybook/index.js
+++ .storybook/index.js
@@ -12,7 +12,8 @@ import {
   select
 } from '@storybook/addon-knobs/react'
 
-import Popper from '../src/index.js'
+/* import Popper from '../src/index.js'*/
+import Popper from '@d8660091/react-popper'
 
 const placementOptions = {
   top: 'Top',
